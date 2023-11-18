2c01f3b33d86894e9c914e7598baefd7452cb487
Josh Frost <jwfrost2002@gmail.com>
2023-11-18 16:43:03 +0000

    Create SpaceRenderer, implement renderer switching

--- Blank Project/Coursework Scene.cpp
+++ Blank Project/Coursework Scene.cpp
@@ -1,5 +1,7 @@
 #include "../NCLGL/window.h"
 #include "Renderer.h"
+#include "SpaceRenderer.h"
+#include "../nclgl/OGLRenderer.h"
 
 int main()	{
 	Window w("Coursework Scene", 1280, 720, false);
@@ -8,21 +10,45 @@ int main()	{
 		return -1;
 	}
 	
-	Renderer renderer(w);
-	if(!renderer.HasInitialised()) {
+	OGLRenderer* renderer = new Renderer(w);	// careful with performance - renderer created on heap not stack
+	if(!(*renderer).HasInitialised()) {
 		return -1;
 	}
+
+	bool useSpaceRenderer = false;
 
 	w.LockMouseToWindow(true);
 	w.ShowOSPointer(false);
 
-	while(w.UpdateWindow()  && !Window::GetKeyboard()->KeyDown(KEYBOARD_ESCAPE)){
-		renderer.UpdateScene(w.GetTimer()->GetTimeDeltaSeconds());
-		renderer.RenderScene();
-		renderer.SwapBuffers();
+
+	while(w.UpdateWindow() && !Window::GetKeyboard()->KeyDown(KEYBOARD_ESCAPE)) {
+		(*renderer).UpdateScene(w.GetTimer()->GetTimeDeltaSeconds());
+		(*renderer).RenderScene();
+		(*renderer).SwapBuffers();
+
+		if (Window::GetKeyboard()->KeyDown(KEYBOARD_Z)) {	// toggle renderer with z key
+			if (!useSpaceRenderer) {
+				delete renderer;
+				renderer = new SpaceRenderer(w);
+				if (!(*renderer).HasInitialised()) {
+					return -1;
+				}
+			}
+			else {
+				delete renderer;
+				renderer = new Renderer(w);
+				if (!(*renderer).HasInitialised()) {
+					return -1;
+				}
+			}
+			useSpaceRenderer = !useSpaceRenderer;
+		}
+		
+
 		if (Window::GetKeyboard()->KeyDown(KEYBOARD_F5)) {
 			Shader::ReloadAllShaders();
 		}
 	}
+	delete renderer;
 	return 0;
 }
